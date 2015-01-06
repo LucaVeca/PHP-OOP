9117a775206108ec73222fcc13298366122a0c32
Luca Veca <lucaveca@gmail.com>
2015-01-06 11:16:17 -0800

    Lecture examples

---
+++ index.php
@@ -0,0 +1,47 @@
+class Soccer{
+	//class player
+	//class ball
+	//class goal
+}
+
+$soccer1 = newSoccer();
+$soccer2 = newSoccer();
+
+class Soccer {
+	public $name="player";
+	public $number=13;
+	public $team="Juventus";
+	public $club="Serie A";
+}
+////////////////////////////////////////////////////////////// /\///\/_//\ /////////////////////////////////////////////////////////////
+class Music{
+	//class Instrument
+	//class Genre
+	//class Volume
+}
+
+$music1 = newMusic();
+$music2 = newMusic();
+
+class Soccer {
+	private $Band="Led Zepplin";
+	private $song="Stairway to Heaven";
+	private $genre="Rock";
+	private $Members=4;
+}
+//////////////////////////////////////////////////////////////// /\///\/_//\ ///////////////////////////////////////////////////////////
+class Nature{
+	//class plant
+	//class food
+	//class tree
+}
+
+$nature1 = newNature();
+$nature2 = newNature();
+
+class Nature {
+	protected $tree="oak";
+	protected $fruit="apple";
+	protected $vegetable="Artichoke";
+	protected $flower="Cherry Blossom";
+}
